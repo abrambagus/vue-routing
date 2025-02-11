e0f78556347bf51d3ffd44e5ea38298384f56356
abram <abrambagus@gmail.com>
2025-02-11 16:33:32 +0700

    organizing route

--- src/main.js
+++ src/main.js
@@ -1,61 +1,7 @@
 import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
-import TeamsList from './components/teams/TeamsList.vue';
-import UsersList from './components/users/UsersList.vue';
-import TeamMembers from './components/teams/TeamMembers.vue';
-import NotFound from './components/nav/NotFound.vue';
-import TeamsFooter from './components/teams/TeamsFooter';
-import UsersFooter from './components/users/UsersFooter';
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { path: '/', redirect: '/teams' },
-    {
-      name: 'teams',
-      path: '/teams',
-      components: {
-        default: TeamsList,
-        footer: TeamsFooter,
-      },
-      children: [
-        {
-          name: 'team-members',
-          path: ':teamId',
-          component: TeamMembers,
-          props: true,
-        },
-      ],
-    },
-    {
-      path: '/users',
-      components: { default: UsersList, footer: UsersFooter },
-      beforeEnter(to, from, next) {
-        console.log(to, from);
-        next();
-      },
-    },
-    { path: '/:notFound(.*)', component: NotFound },
-  ],
-  linkActiveClass: 'active',
-  scrollBehavior(_, _2, savedPostition) {
-    if (savedPostition) {
-      return savedPostition;
-    }
-    return { left: 0, top: 0 };
-  },
-});
-
-router.beforeEach(function (to, from, next) {
-  console.log(to, from);
-  next();
-});
-
-router.afterEach(function (to, from) {
-  console.log(to, from);
-});
+import router from './router.js';
 
 const app = createApp(App);
 
